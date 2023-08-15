1b7f97a47f95d851ea6108b6c90c96814febe7f1
AlitalipSever <alitalipsever@gmail.com>
2023-08-16 01:20:34 +0200

    test(TweetComposeArea): render tests add

---
+++ src/components/organisms/TweetComposeArea/TweetComposeArea.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TweetComposeArea from './index';
+
+describe('TweetComposeArea', () => {
+  const mockOnChange = jest.fn();
+  const mockOnSubmit = jest.fn();
+
+  beforeEach(() => {
+    render(
+      <TweetComposeArea
+        tweetText=""
+        onChange={mockOnChange}
+        onSubmit={mockOnSubmit}
+      />,
+    );
+  });
+
+  it('renders without crashing', () => {
+    const placeholder = screen.getByPlaceholderText("What's happening?");
+    expect(placeholder).toBeInTheDocument();
+  });
+
+  it('calls onChange handler when input value changes', () => {
+    const inputElement = screen.getByPlaceholderText("What's happening?");
+    fireEvent.change(inputElement, { target: { value: 'Testing tweet' } });
+
+    expect(mockOnChange).toHaveBeenCalled();
+  });
+
+  it('calls onSubmit when "Post" button is clicked', () => {
+    const buttonElement = screen.getByText('Post');
+    fireEvent.click(buttonElement);
+
+    expect(mockOnSubmit).toHaveBeenCalled();
+  });
+});
