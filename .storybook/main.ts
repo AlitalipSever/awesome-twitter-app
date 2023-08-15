6dd2b62c27d37b94b8a9fcd2531fc97fbc2872c8
AlitalipSever <alitalipsever@gmail.com>
2023-08-15 20:26:21 +0200

    fix(storybook): tailwind styling on storybook ui fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: {
-          implementation: require.resolve('postcss'),
-        },
-      },
-    },
   ],
   framework: {
     name: '@storybook/react-webpack5',
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,5 @@
 import type { Preview } from '@storybook/react';
-
-import { withThemeByClassName } from '@storybook/addon-styling';
-
-/* TODO: update import to your tailwind styles file. If you're using Angular, inject this through your angular.json config instead */
-import '../src/index.css';
+import "../src/index.css"
 
 const preview: Preview = {
   parameters: {
@@ -15,18 +11,6 @@ const preview: Preview = {
       },
     },
   },
-
-  decorators: [
-    // Adds theme switching support.
-    // NOTE: requires setting "darkMode" to "class" in your tailwind config
-    withThemeByClassName({
-      themes: {
-        light: 'light',
-        dark: 'dark',
-      },
-      defaultTheme: 'light',
-    }),
-  ],
 };
 
 export default preview;
