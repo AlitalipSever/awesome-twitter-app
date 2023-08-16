9092dc55ea35c941991c01d39583b0d64506c264
AlitalipSever <alitalipsever@gmail.com>
2023-08-16 16:56:13 +0200

    feat(tweet): ui friendly timestamp add

--- src/services/TweetService/utils.ts
+++ src/services/TweetService/utils.ts
@@ -1,23 +1,24 @@
 import { interval } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Tweet } from './types';
+import moment from 'moment';
 
 export const createTweetSource = (
   frequency: number,
   account: string,
   attribute: string,
+  URL: string,
 ) => {
   return interval(frequency).pipe(
     map(
       (i: number) =>
         ({
           account,
-          timestamp: Date.now().toString(),
+          timestamp: moment(Date.now()).format('HH:mm:ss'),
           content: `${attribute} Tweet number ${i + 1}`,
           username: `${attribute}User${i + 1}`,
           handle: `${attribute}User${i + 1}`,
-          avatarUrl:
-            'https://pbs.twimg.com/profile_images/517863945/mattsailing_400x400.jpg',
+          avatarUrl: URL,
         }) as Tweet,
     ),
   );
