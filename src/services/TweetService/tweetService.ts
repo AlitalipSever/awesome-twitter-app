99688dd2fd0dfef5efaabb70b9268f762420db62
AlitalipSever <alitalipsever@gmail.com>
2023-08-16 16:56:45 +0200

    feat(tweet): different pp add for better app exp.

--- src/services/TweetService/tweetService.ts
+++ src/services/TweetService/tweetService.ts
@@ -2,9 +2,24 @@ import { merge } from 'rxjs';
 import { createTweetSource } from './utils';
 
 const tweetObservable = merge(
-  createTweetSource(5000, 'AwardsDarwin', 'Facepalm'),
-  createTweetSource(3000, 'iamdevloper', 'Expert'),
-  createTweetSource(5000, 'CommitStrip', 'Funny'),
+  createTweetSource(
+    5000,
+    'AwardsDarwin',
+    'Facepalm',
+    'https://pbs.twimg.com/profile_images/1683325380441128960/yRsRRjGO_400x400.jpg',
+  ),
+  createTweetSource(
+    3000,
+    'iamdevloper',
+    'Expert',
+    'https://pbs.twimg.com/profile_images/1677090954350583811/Xy93qVY4_400x400.jpg',
+  ),
+  createTweetSource(
+    5000,
+    'CommitStrip',
+    'Funny',
+    'https://pbs.twimg.com/profile_images/517863945/mattsailing_400x400.jpg',
+  ),
 );
 
 export const tweetService = {
