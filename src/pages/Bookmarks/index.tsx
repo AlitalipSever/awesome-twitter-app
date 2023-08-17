77e668f8df265741a19582f9a94aea988e3aeb45
AlitalipSever <alitalipsever@gmail.com>
2023-08-17 18:46:29 +0200

    feat(pages): new pages add with routes

---
+++ src/pages/Bookmarks/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bookmarks: React.FC = () => {
+  return <div>Bookmarks View</div>;
+};
+
+export default Bookmarks;
---
+++ src/pages/Messages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Messages: React.FC = () => {
+  return <div>Messages View</div>;
+};
+
+export default Messages;
