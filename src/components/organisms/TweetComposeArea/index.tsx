e9abb4892140e46f72aecedaa6aa43a9c310443f
AlitalipSever <alitalipsever@gmail.com>
2023-08-26 13:23:24 +0200

    feat(tweet-compose): enhance TweetComposeArea and TweetComposer components
    - Update TweetComposeAreaProps to accept avatarUrl and username
    - Replace input with textarea for multi-line tweet support
    - Add focus and transition styles for textarea
    - Utilize constants AVATAR_URL and USERNAME from utils
    - Adjust handleTextChange for textarea event type

--- src/components/organisms/TweetComposeArea/index.tsx
+++ src/components/organisms/TweetComposeArea/index.tsx
@@ -4,30 +4,33 @@ import UserAvatar from '../../atoms/UserAvatar';
 
 export interface TweetComposeAreaProps {
   tweetText: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onSubmit: () => void;
+  avatarUrl: string;
+  username: string;
 }
+
 const TweetComposeArea: React.FC<TweetComposeAreaProps> = ({
   onChange,
   onSubmit,
   tweetText,
+  avatarUrl,
+  username,
 }) => {
   return (
     <div className="border-t border-b border-gray-300 h-40 flex-row flex px-4">
       <div className="w-16 pt-4">
-        <UserAvatar
-          avatarUrl={'https://via.placeholder.com/150'}
-          username={'ats'}
-        />
+        <UserAvatar avatarUrl={avatarUrl} username={username} />
       </div>
       <div className="flex flex-col w-full">
-        <div className="border-b border-gray-300 h-32">
-          <input
-            type="text"
+        <div className="border-b border-gray-300 h-32 pt-4">
+          <textarea
             placeholder="What's happening?"
             value={tweetText}
             onChange={onChange}
-            className="w-full h-16 outline-none border-none py-4"
+            className="w-full h-16 outline-none border-none py-4 pl-3 resize-none focus:bg-blue-100 transition duration-200"
+            aria-label="Compose tweet"
+            rows={4}
           />
         </div>
         <TweetToolbar onPost={onSubmit} />
---
+++ src/utils/index.tsx
@@ -0,0 +1,3 @@
+export const AVATAR_URL =
+  'https://pbs.twimg.com/profile_images/1621599467853021184/5t0agsFM_400x400.jpg';
+export const USERNAME = 'Ali';
